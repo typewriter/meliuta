ad579f828c485c452f67c29ece009332418527f8
typewriter <takuyaxp@gmail.com>
2021-05-12 22:17:26 +0900

    Separate LatestSong component

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -22,52 +22,7 @@ import {
 } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import CardMedia from '@material-ui/core/CardMedia';
-
-const songUrl = "/songs.tsv";
-
-const latestSongs: {
-    title: string;
-    embedUrl: string;
-}[] = [
-        {
-            title: "エンヴィーベイビー",
-            embedUrl: "https://www.youtube.com/embed/xz0iriPFNcA",
-        },
-        {
-            title: "celluloid",
-            embedUrl: "https://www.youtube.com/embed/8CUTuw-xJgc",
-        },
-        {
-            title: "メビウス",
-            embedUrl: "https://www.youtube.com/embed/h2YDbhEURZA",
-        },
-        {
-            title: "レッド・パージ！！！",
-            embedUrl: "https://www.youtube.com/embed/3JU6I12H5WU",
-        },
-        {
-            title: "KING",
-            embedUrl: "https://www.youtube.com/embed/X5VEzYA1C-8",
-        },
-        {
-            title: "就寝御礼",
-            embedUrl: "https://www.youtube.com/embed/StA3_Nc5dzM",
-        },
-    ];
-
-const originalSongs: {
-    title: string;
-    embedUrl: string;
-}[] = [
-        {
-            title: "<strong>胎生</strong><br />うたさっきょくさくしみっくす：メリッサ・キンレンカ<br />絵：ととりか<br />編曲：友人１２３４５６７８９０",
-            embedUrl: "https://www.youtube.com/embed/dlVWaAQZPag",
-        },
-        {
-            title: "<strong>haze</strong><br/ >歌・作詞・作曲・mix：メリッサ・キンレンカ<br />イラスト：ohuton<br />編曲：友人ABCDEFG",
-            embedUrl: "https://www.youtube.com/embed/hhXeNNxovSE",
-        },
-    ];
+import LatestSong from './LatestSong';
 
 // const songMap: Map<string, number> = new Map([
 //   ["動画", 1],
@@ -155,40 +110,7 @@ const Home = () => {
                 :
                 <>
                 </>}
-            <Container maxWidth="lg" className="s-container">
-                <Typography component="h6" variant="h6">
-                    <FiberNew /> 最新の歌動画
-                </Typography>
-                <Grid container spacing={4}>
-                    {latestSongs.slice(0, 1).map((song) => (
-                        <Grid item key={song.title} xs={12}>
-                            <Card>
-                                <CardContent>
-                                    {song.title}
-                                    <LazyYouTubePlayer title={song.title} url={song.embedUrl} />
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Container>
-            <Container maxWidth="lg" className="s-container">
-                <Typography component="h6" variant="h6">
-                    <LibraryMusic /> オリジナル曲
-                </Typography>
-                <Grid container spacing={4}>
-                    {originalSongs.slice(0, 2).map((song) => (
-                        <Grid item key={song.title} xs={12} md={6}>
-                            <Card>
-                                <CardContent>
-                                    <span dangerouslySetInnerHTML={{ __html: song.title }} />
-                                    <LazyYouTubePlayer title={song.title} url={song.embedUrl} />
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Container>
+            <LatestSong />
         </main>
     );
 };
